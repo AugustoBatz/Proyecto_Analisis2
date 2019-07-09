f17bb9ea189e7b9a40dc0153306fd6e4a0aaa3fa
chris <caqp14@gmail.com>
2019-07-09 09:40:07 -0600

    Singleton DB
    Singleton DB

--- InventarioMaven/src/main/java/factorybd/FactoryBaseDeDatos.java
+++ InventarioMaven/src/main/java/factorybd/FactoryBaseDeDatos.java
@@ -16,16 +16,35 @@ import javax.persistence.TypedQuery;
  * @author chr
  */
 public class FactoryBaseDeDatos {
-        
+    private static FactoryBaseDeDatos conexionDb;
+    private EntityManagerFactory emf;
+    private EntityManager em;    
     
     public FactoryBaseDeDatos(){
         //nombre base de datos
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("restaurante");
-        EntityManager em = emf.createEntityManager();
+        emf = Persistence.createEntityManagerFactory("restaurante");
+        em = emf.createEntityManager();
     }
-    public     
+    public static FactoryBaseDeDatos getInstancia() {
+        if (conexionDb == null) {
+            conexionDb = new FactoryBaseDeDatos();
+        }
+        return conexionDb;
+    }
+
+    public boolean EstadoConexionDb() {
+        return em.isOpen();
+    }
+
+    public String cerrarConexionDb() {
+        this.em.close();
+        return "Desconectado";
+    }
+
+    public EntityManager getEntityManager() {
+        return this.em;
+    }
         
-        //TypedQuery<Cafe> query = em.createNamedQuery("Cafe.findAll", Cafe.class);
-       // List<Cafe> listaCafes = query.getResultList();
+
     
 }
