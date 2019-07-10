21de389644ee5303c82f77f5ef77908f53ea1d8e
chris <caqp14@gmail.com>
2019-07-09 23:29:22 -0600

    cambios
    cambios

--- InventarioMaven/src/main/java/gt/url/edu/proveedorespackage/VentanaProveedores.java
+++ InventarioMaven/src/main/java/gt/url/edu/proveedorespackage/VentanaProveedores.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proveedorespackage;
+package gt.url.edu.proveedorespackage;
 
 /**
  *
